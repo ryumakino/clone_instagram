8093b9279d0d1c38bd335b26f9b758acd60404fa
ryumakino <ricardoryu.makino@gmail.com>
2025-01-15 10:00:46 -0300

    turbo_likeDislike

--- app/javascript/controllers/like_dislike_controller.js
+++ app/javascript/controllers/like_dislike_controller.js
@@ -3,55 +3,26 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["like", "dislike"];
 
-  // CSRF token for API requests
-  get csrfToken() {
-    return document.querySelector("meta[name='csrf-token']").content;
-  }
-
-  get headers() {
-    return {
-      "Content-Type": "application/json",
-      "X-CSRF-Token": this.csrfToken
-    };
-  }
-
-  // Event handler for clicking the like button
-  like(event) {
+  toggleLikeDislike(event) {
     const actionElement = event.target.closest(".action");
+    const postId = actionElement.dataset.postId;
+    const likeId = actionElement.dataset.likeId;
+    const method = likeId ? "DELETE" : "POST";
+    const url = likeId ? `/likes/${likeId}` : "/likes";
 
-    fetch("/likes", {
-      method: "POST",
-      headers: this.headers,
-      body: JSON.stringify({ like: { post_id: actionElement.dataset.postId } })
-    })
-      .then(response => response.json())
-      .then(json => this.handleLikeFeatureCallback({ ...json, actionElement }))
-      .catch(error => console.log("Error:", error));
-  }
+    const form = document.createElement("form");
+    form.method = method;
+    form.action = url;
 
-  // Event handler for clicking the dislike button
-  dislike(event) {
-    const actionElement = event.target.closest(".action");
-
-    fetch(`/likes/${actionElement.dataset.likeId}`, {
-      method: "DELETE",
-      headers: this.headers
-    })
-      .then(response => response.json())
-      .then(json => this.handleLikeFeatureCallback({ ...json, actionElement }))
-      .catch(error => console.log("Error:", error));
-  }
-
-  // Toggle like/dislike buttons based on the response
-  handleLikeFeatureCallback({ id, successful, actionElement }) {
-    if (successful) {
-      // Update the likeId in the dataset
-      actionElement.dataset.likeId = id;
-
-      // Toggle visibility of like and dislike buttons
-      actionElement.querySelectorAll(".like, .dislike").forEach(button => {
-        button.classList.toggle("hidden");
-      });
+    if (!likeId) {
+      const postIdInput = document.createElement("input");
+      postIdInput.type = "hidden";
+      postIdInput.name = "like[post_id]";
+      postIdInput.value = postId;
+      form.appendChild(postIdInput);
     }
+
+    document.body.appendChild(form);
+    form.submit();
   }
 }
